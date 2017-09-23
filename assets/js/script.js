823ed2965cdc12531103c350f2974db5a65bb052
System Administrator <root@Marcelos-MacBook-Air.local>
2017-09-23 15:46:30 -0300

    VQV

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,16 @@
 
 var myApp = angular.module('xpAula01', []);
 
-myApp.controller('MainCtrl', ['$scope', function($scope) {
+myApp.controller('MainCtrl', ['$scope', '$window', function($scope, $window) {
       
 	$scope.data = {};
 	$scope.data.name = "";
 	$scope.data.email = "";	
 
-	
+	$scope.goToThankYou = function(){
+		console.log("00000")
+		window.location.href = "/thank_you.html";
+	}
 
 }]);
 })(window.angular);
